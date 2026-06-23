b93752be028fd79d3f5340534679c13d51a5dc6a
maikonadvogadomg-alt <maikon.advogado.mg@gmail.com>
2026-06-23 13:38:20 -0300

    fix: metro exclui artifacts/ do bundle (previne erro de bundle)

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
+  const path = require("path");
 
-module.exports = getDefaultConfig(__dirname);
+  const config = getDefaultConfig(__dirname);
+
+  // Exclui pastas que não são React Native (API server, code editor, etc.)
+  config.watchFolders = [__dirname];
+  config.resolver = {
+    ...config.resolver,
+    blockList: [
+      /artifacts[\/]api-server[\/].*/,
+      /artifacts[\/]code-editor[\/].*/,
+      /devmobile-fix[\/].*/,
+      /server[\/].*/,
+    ],
+  };
+
+  module.exports = config;
+  
